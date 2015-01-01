0db578e9e3330d2dc17744cf22d9e28eada47b35
Ragunath Jawahar <rj@mobsandgeeks.com>
2015-01-01 19:30:39 +0530

    [Book Store Reflection] First table model using reflection.

---
+++ Section 1/Book Store Reflection/src/main/java/com/codeherenow/bookstore/books/Book.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright 2014, Ragunath Jawahar (www.codeherenow.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.codeherenow.bookstore.books;
+
+/**
+ * POJO representing a Book entity.
+ *
+ * @author Ragunath Jawahar <www.codeherenow.com>
+ */
+public class Book {
+    private String title;
+    private String author;
+    private String isbn;
+    private double price;
+
+    public Book(String title, String author, String isbn, double price) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
